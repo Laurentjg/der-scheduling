b8042c9344dc90887abb5711bf99f46eaf4e48c9
Michael Zillgith <michael.zillgith@mz-automation.de>
2022-06-28 23:03:17 +0100

    - added first code to handle schedules (FSCH LNs)
    Signed-off-by: Michael Zillgith <michael.zillgith@mz-automation.de>

---
+++ examples/scheduler_example1.c
@@ -0,0 +1,47 @@
+#include "der_scheduler.h"
+
+#include <signal.h>
+#include <stdio.h>
+
+static bool running = false;
+
+static void
+sigint_handler(int signalId)
+{
+    running = false;
+}
+
+int
+main(int argc, char** argv)
+{
+    IedModel* model = ConfigFileParser_createModelFromConfigFileEx("model.cfg");
+
+    if (model) {
+        IedServer server = IedServer_create(model);
+
+        Scheduler sched = Scheduler_create(model, server);
+
+        Scheduler_parseModel(sched);
+
+        IedServer_start(server, 102);
+
+        if (IedServer_isRunning(server)) {
+
+            running = true;
+
+            while (running) {
+                Thread_sleep(100);
+            }            
+        }
+        else {
+            printf("ERROR: Cannot start server\n");
+        }
+
+        IedServer_destroy(server);
+        IedModel_destroy(model);
+    }
+    else {
+        printf("ERROR: Failed to load data model\n");
+    }
+
+}
